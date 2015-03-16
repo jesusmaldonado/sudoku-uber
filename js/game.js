8a01154f868832ff7822c6d53f98891c9a7a4119
Jesus Maldonado <jesusmaldonado317@gmail.com>
2015-03-16 07:03:28 -0400

    adds difficulty changes

--- js/game.js
+++ js/game.js
@@ -13,11 +13,40 @@
   };
 
   Game.prototype.installEvents = function() {
-      $(".play-again").on("click", this.newGame.bind(this));
-
+      $(".play-again").on("click", this.playAgain.bind(this));
+      $(".control-buttons").on("click", "a", this.changeDifficulty.bind(this));
   };
 
-  Game
+  Game.prototype.playAgain = function() {
+    var difficulty = $(".current-difficulty").html()
+    this.resetBoard(difficulty);
+    $(".winning-message").addClass("hidden");
+
+
+  }
+
+  Game.prototype.changeDifficulty = function(event){
+    var controls = $(".control-buttons").find("a").removeClass("current-difficulty")
+    var newDifficulty = $(event.currentTarget)
+    newDifficulty.addClass("current-difficulty")
+    var difficulty = newDifficulty.html();
+    this.resetBoard(difficulty);
+  };
+
+  Game.prototype.resetBoard = function(difficulty) {
+    if (difficulty === "baby mode") {
+      difficulty = 80;
+    }
+    this.$container.empty();
+    this.$container.removeClass("inactive");
+    var str = sudoku.generate(difficulty);
+    var inputString = str.replace(/\./g, "0");
+    var answerString = sudoku.solve(str);
+
+    this.board = new Sudoku.Board(this, this.$container, inputString, answerString);
+
+  }
+
 
   Game.prototype.won = function() {
     var message = $(".winning-message");
